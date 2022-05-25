e26e7fa1e2be43cd3936c65664b39240b8d1b308
Locandjer <alessio.grim@gmail.com>
2022-05-25 12:08:07 +0200

    commit per serie terminator

--- modulo_api.js
+++ modulo_api.js
@@ -1,6 +1,12 @@
 const URL_LIST_TOY =
 "https://www.omdbapi.com/?apikey=af44033&s=toyboy";
 
+const URL_LIST_TERMINATOR =
+"https://www.omdbapi.com/?apikey=af44033&s=Terminator&type=series";
+/*
+    chiama la web api con l'elenco di tutti i contenuti di "toyboy"
+*/
+
 export const listToyboy = () => {
     fetch(URL_LIST_TOY)
     .then((response) => response.json())
@@ -9,3 +15,16 @@ export const listToyboy = () => {
         console.log(film);
     });
 };
+
+export const listTerminatorSerie = () => {
+    fetch(URL_LIST_TERMINATOR)
+    .then((response) => response.json())
+    .then((result) => {
+        const serie = result.Search;
+        console.log(serie);
+    });
+};
+
+/*
+    chiama la web api e restituisce le serie televisive di "toyboy"
+*/
